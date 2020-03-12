e7f3c2a5d339d009696c2fe5c5e9f8b4913965b1
Alison Law <alisparklaw@gmail.com>
2020-03-12 11:08:28 +0000

    static  folder build

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 require("dotenv").config()
+
+//specify a folder for static assets, like images, css, index.hyml, js ....
+//we need an endpoint on "/" to send index.html
+const path = require("path")
+
 const express = require('express')
 const cors = require('cors')
 const app = express()
+//middleware
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname, "client/build")))
+
 const PORT = process.env.PORT || 9000
 
 
 app.get('/api', (req, res) => {
   res.json({ message: 'The api is UP' })
 })
+
+// fallback endpoint that will just send back index.html with the CRA
+app.get("*", (req, res) => {
+    // send back the index.html contained inside client/build
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
+  
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 })
